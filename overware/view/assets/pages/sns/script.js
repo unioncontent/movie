7e76dfa7a9b6088ae16026cc15e61b1695fb54b4
suyeon1457 <smb1457@naver.com>
2017-10-30 16:17:21 +0900

    sns관리 페이지 엑셀버튼 추가

--- overware/view/assets/pages/sns/script.js
+++ overware/view/assets/pages/sns/script.js
@@ -7,6 +7,21 @@ $(window).on('resize',function(){
 
 $(document).ready(function() {
   linechart();
+  //일괄처리 확인메시지
+	document.querySelector('.alert-confirm').onclick = function(){
+		swal({
+					title: "엑셀출력 하시겠습니까?",
+					text: "현재 리스트가 엑셀출력 됩니다.",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonClass: "btn-danger",
+					confirmButtonText: "YES",
+					closeOnConfirm: false
+				},
+				function(){//엑셀 출력하겠다고 할 시 진행 함수
+					swal("Success!", "엑셀출력 되었습니다.", "success");
+				});
+	};
 });
 
 /*line*/
