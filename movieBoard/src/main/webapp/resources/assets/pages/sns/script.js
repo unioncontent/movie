2240d788aaabd80d25d193795a578e774667a16c
suyeon1457 <smb1457@naver.com>
2017-10-30 16:30:28 +0900

    assets폴더에 파일 추가

--- movieBoard/src/main/webapp/resources/assets/pages/sns/script.js
+++ movieBoard/src/main/webapp/resources/assets/pages/sns/script.js
@@ -1,26 +1,41 @@
-/*'use strict';*/
-/*$(window).on('resize',function(){
+'use strict';
+$(window).on('resize',function(){
   setTimeout(function(){
     window.lineChart.redraw();
   }, 500);
-});*/
+});
 
-/*$(document).ready(function() {
+$(document).ready(function() {
   linechart();
-});*/
+  //일괄처리 확인메시지
+	document.querySelector('.alert-confirm').onclick = function(){
+		swal({
+					title: "엑셀출력 하시겠습니까?",
+					text: "현재 리스트가 엑셀출력 됩니다.",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonClass: "btn-danger",
+					confirmButtonText: "YES",
+					closeOnConfirm: false
+				},
+				function(){//엑셀 출력하겠다고 할 시 진행 함수
+					swal("Success!", "엑셀출력 되었습니다.", "success");
+				});
+	};
+});
 
 /*line*/
-/*function linechart(){
+function linechart(){
   window.lineChart = Morris.Line({
       element: 'line-chart1',
       data: [
-        {period: '2017-10-10',l1: 10,l2: 30,l3: 10},
-        {period: '2017-10-11',l1: 20,l2: 10,l3: 20},
-        {period: '2017-10-12',l1: 30,l2: 30,l3: 30},
-        {period: '2017-10-13',l1: 40,l2: 10,l3: 40},
-        {period: '2017-10-14',l1: 50,l2: 30,l3: 50},
-        {period: '2017-10-15',l1: 60,l2: 10,l3: 60},
-        {period: '2017-10-16',l1: 70,l2: 30,l3: 70}
+        {period: '2017-10-10',l1: 0,l2: 0,l3: 0},
+        {period: '2017-10-11',l1: 50,l2: 15,l3: 5},
+        {period: '2017-10-12',l1: 20,l2: 50,l3: 65},
+        {period: '2017-10-13',l1: 60,l2: 12,l3: 7},
+        {period: '2017-10-14',l1: 30,l2: 20,l3: 120},
+        {period: '2017-10-15',l1: 25,l2: 80,l3: 40},
+        {period: '2017-10-16',l1: 10,l2: 10,l3: 10}
       ],
       xkey: 'period',
       redraw: true,
@@ -29,4 +44,4 @@
       labels: ['좋아요', '공유', '댓글'],
       lineColors: ['#fb9678', '#7E81CB', '#01C0C8']
   });
-}*/
+}
