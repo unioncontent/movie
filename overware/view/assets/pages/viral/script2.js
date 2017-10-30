5706297e06231c30b9695a7f376948e48adeaca1
suyeon1457 <smb1457@naver.com>
2017-10-30 18:07:49 +0900

    엑셀버튼 추가중

--- overware/view/assets/pages/viral/script2.js
+++ overware/view/assets/pages/viral/script2.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
+  $('#time').timepicker({
+    timeFormat: 'h:mm p',
+    interval: 60,
+    minTime: '12:00am',
+    maxTime: '11:00pm',
+    defaultTime: '12:00am',
+    startTime: '12:00am',
+    dynamic: true,
+    dropdown: true,
+    scrollbar: true
+  });
 });
