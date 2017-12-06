debd33c7f2e98b5a72a0da674321ec4edeb227b7
Administrator <Administrator@WIN-TO3Q4LD8U4S>
2017-12-06 13:46:07 +0900

    모니터링현황 완료.

--- movieBoard/src/main/java/org/union/service/MonitorService.java
+++ movieBoard/src/main/java/org/union/service/MonitorService.java
@@ -1,10 +1,14 @@
 package org.union.service;
 
+import java.util.List;
+
 import org.union.domain.MonitorVO;
 
 public interface MonitorService {
 	
 	
 	public void insert(MonitorVO vo);
+	
+	public List<MonitorVO> pageAll();
 
 }
