482521878aba582d6a3e3c8727b72bdaacae9a7c
sangw <sangw@DESKTOP-P9B8MJU>
2017-12-27 02:09:11 +0900

    portal 업로드 테스트 완료.

--- movieBoard/src/main/java/org/union/controller/UploadController.java
+++ movieBoard/src/main/java/org/union/controller/UploadController.java
@@ -1,36 +1,43 @@
 package org.union.controller;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.util.UUID;
 
 import javax.annotation.Resource;
 
 import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.union.domain.PortalVO;
+import org.union.service.CommunityService;
+import org.union.service.MediaService;
+import org.union.service.PortalService;
 import org.union.util.UploadFileUtils;
 
 @Controller
 @RequestMapping
 public class UploadController {
+	
+	@Autowired
+	private PortalService portalService;
+	
+	@Autowired
+	private MediaService mediaService;
+	
+	@Autowired
+	private CommunityService communityService;
 
 	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 	
-	@PostMapping("/uploadFile")
+	/*@PostMapping("/uploadFile")
 	@ResponseBody
 	public void uploadFile(MultipartFile file, Model model) throws Exception {
 
@@ -48,7 +55,7 @@ public class UploadController {
 		
 		logger.info("uploadName :"+uploadName);
 
-	}
+	}*/
 
 	@GetMapping(value = "classification/show", produces = {"image/jpg", "image/jpeg" })
 	public @ResponseBody byte[] show(String name) throws Exception {
@@ -66,7 +73,9 @@ public class UploadController {
 
 	@ResponseBody
 	@RequestMapping(value = "/uploadAjax", method = RequestMethod.POST, produces = "text/plain;charset=UTF-8")
-	public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception {
+	public ResponseEntity<String> uploadAjax(MultipartFile file, String domain, String idx) throws Exception {
+		logger.info("domain: " + domain);
+		logger.info("idx: " + idx);
 		logger.info("originalName: " + file.getOriginalFilename());
 		logger.info("size: " + file.getSize());
 		logger.info("contentType: " + file.getContentType());
@@ -74,12 +83,42 @@ public class UploadController {
 		
 //		return new ResponseEntity<String>(file.getOriginalFilename(),HttpStatus.CREATED);
 
-		logger.info(UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()),
-				HttpStatus.CREATED);
+		/*logger.info(UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()),
+				HttpStatus.CREATED);*/
 		
-		return new ResponseEntity<String>(
-				UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()),
-				HttpStatus.CREATED);
+		ResponseEntity<String> insertFileName;
+		
+		if(domain.equals("portal")) {
+			logger.info("domain is portal");
+			PortalVO vo = new PortalVO();
+			
+			insertFileName = new ResponseEntity<String>(
+					UploadFileUtils.uploadFile(uploadPath, file.getOriginalFilename(), file.getBytes()),
+					HttpStatus.CREATED);
+			
+			
+			vo.setPortal_idx(Integer.parseInt(idx));
+			vo.setThumbnail(insertFileName.getBody());
+			
+			logger.info("vo: " + vo);
+			
+			portalService.modifyThumbnail(vo);
+			
+			return insertFileName;
+			
+		}else if(domain.equals("media")) {
+			
+			return null;
+			
+		}else if(domain.equals("community")) {
+			
+			return null;
+			
+		}else {
+			return null;
+		}
+		
+		
 	}
 	
 }
