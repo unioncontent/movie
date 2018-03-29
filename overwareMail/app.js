36a2cdb20005baf710a53f3d9bfa20c9bda45024
BYUN SOOYEON <smb1457@naver.com>
2018-03-29 13:30:54 +0900

    [ mail ] 사이트 틀, view단 설정

--- overwareMail/app.js
+++ overwareMail/app.js
@@ -4,26 +4,49 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+// 추가
+var expressLayouts = require('express-ejs-layouts');
+var session = require('express-session');
+var passport = require('passport');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// ejs-layouts setup
+app.set('layout', 'layout/layout');
+app.set('layout extractScripts', true);
+app.set('layout extractStyles', true);
+// middlewares setup
+app.use(expressLayouts);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// login/logout setup
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(session({
+  secret: '1@%24^%$3^*&98&^%$', // 쿠키에 저장할 connect.sid값을 암호화할 키값 입력
+  resave: false, //세션 아이디를 접속할때마다 새롭게 발급하지 않음
+  saveUninitialized: true, //세션 아이디를 실제 사용하기전에는 발급하지 않음
+  cookie: {
+    maxAge: 1000 * 60 * (60*3) // 쿠키 유효기간 3시간
+  }
+}));
+// routes setup
+var index = require('./routes/index');
 app.use('/', index);
-app.use('/users', users);
+app.use('/login', index);
+var period = require('./routes/period');
+app.use('/period', period);
+var email = require('./routes/email');
+app.use('/email', email);
+var list = require('./routes/list');
+app.use('/list', list);
+app.use('/list/add', list);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ overwareMail/db/db_info.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+
+class Database {
+  constructor() {
+    this.db = {
+      host     : '192.168.0.16',
+      user     : 'soas',
+      password : 'qwer1234',
+      port     : '3306',
+      database : 'union'
+    };
+    this.connection = mysql.createConnection(this.db);
+  }
+  query(sql, args) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, args, (err, rows) => {
+        if (err || rows.length == 0)
+          return reject(err);
+        resolve(rows);
+      });
+    });
+  }
+  close() {
+    return new Promise((resolve, reject) => {
+      this.connection.end(err => {
+        if (err)
+          return reject(err);
+        resolve();
+      });
+    });
+  }
+}
+
+module.exports = Database;
