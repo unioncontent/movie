2641731570179ff49f0c310fec49da21971ebd82
BYUN SOOYEON <smb1457@naver.com>
2018-04-09 17:25:15 +0900

    [ mail ] 메일등록 페이지 구현중
    - 리스트페이징, 리스트검색 구현완료

--- overwareMail/routes/list.js
+++ overwareMail/routes/list.js
@@ -5,9 +5,47 @@ var mailListA = require('../models/mailListA.js');
 
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('list');
+router.get('/',async function(req, res) {
+  var data = await getListPageData(req.query);
+  res.render('list',data);
 });
+router.post('/getNextPage',async function(req, res, next) {
+  // if (!req.user) {
+  //   return res.redirect('/login');
+  // }
+  var data = await getListPageData(req.body);
+  res.send(data);
+});
+async function getListPageData(param){
+  var data = {
+    mailList:[],
+    mailListCount:{total:0}
+  };
+  var limit = 20;
+  var searchParam = ['1',0,limit];
+  var currentPage = 1;
+  var searchBody = {};
+  if (typeof param.page !== 'undefined') {
+    currentPage = param.page;
+  }
+  if (parseInt(currentPage) > 0) {
+    searchParam[1] = (currentPage - 1) * limit
+    data['offset'] = searchParam[1];
+  }
+  if (typeof param.searchType !== 'undefined' && typeof param.search !== 'undefined') {
+    searchBody['searchType'] = param.searchType;
+    searchBody['search'] = param.search;
+  }
+  try{
+    data['mailList'] = await mailListA.selectView(searchBody,searchParam);
+    data['mailListCount'] = await mailListA.selectViewCount(searchBody,searchParam);
+    data['currentPage'] = currentPage;
+  }
+  catch(e){
+    console.log('e');
+  }
+  return data;
+}
 
 router.get('/group', function(req, res) {
   res.render('listGroup');
