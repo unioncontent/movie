ad5fd9cddbcc1b7a61f913b2c2df45e2d22b1c76
BYUN SOOYEON <smb1457@naver.com>
2018-04-09 13:06:43 +0900

    [ mail ] 메일등록 페이지 구현중
    - DB연결 및 모달나오는 부분 구현중

--- overwareMail/routes/list.js
+++ overwareMail/routes/list.js
@@ -1,14 +1,31 @@
 var express = require('express');
+// DB module
+var user = require('../models/user.js');
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   res.render('list');
 });
+
 router.get('/group', function(req, res, next) {
   res.render('listGroup');
 });
+
 router.get('/add', function(req, res, next) {
   res.render('listAdd');
+});
+
+router.post('/add/list',async function(req,res,next){
+  var userParam = [0,10];
+  var userList = await user.selectReporter(userParam);
+  res.send('add');
+});
+
+router.post('/add/emailCheck',async function(req,res,next){
+  var emailParam = [req.body.search];
+  var emailCheck = await user.emailCheck(emailParam);
+  res.send(emailCheck);
 });
 
 module.exports = router;
