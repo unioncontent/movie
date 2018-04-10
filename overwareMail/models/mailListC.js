19ff47536e64e1e6f1185fec509d88fb5bf167ed
BYUN SOOYEON <smb1457@naver.com>
2018-04-10 16:21:30 +0900

    [ mail ] 메일그룹 페이지 구현중
    - 페이징완료
    - 검색완료
    - 모달부분 개발중

--- overwareMail/models/mailListC.js
+++ overwareMail/models/mailListC.js
@@ -11,6 +11,11 @@ var mailListC = {
     var sql = insertSqlSetting(Object.keys(param));
     return await getResult(sql,pValue);
   },
+  deleteFun: async function(param){
+    var pValue = Object.values(param);
+    var sql = 'delete from m_mail_list_c where n_idx=?;';
+    return await getResult(sql,pValue);
+  },
   getEmail : async function(param){
     // 로그인 -> M_ID!!!
     var sql = 'select M_email from m_mail_list_c where M_ID=\'1\' ';
@@ -56,8 +61,8 @@ var mailListC = {
     var sql = 'select * from m_mail_list_c where M_idx_a=?';
     return await getResult(sql,[m_idx]);
   },
-  selectView: async function(body,param,callback){
-    var sql = 'select M_ID,user_name,search,count(*) as groupCount,M_group_title,M_group_title';
+  selectView: async function(body,param){
+    var sql = 'select M_ID,date_format(M_regdate, \'%Y-%m-%d %H:%i:%s\') as M_regdate,user_name,search,count(*) as groupCount,M_group_title,M_group_title';
     if (typeof body.as !== 'undefined') {
       sql += body.as+' ,CONCAT(M_group_title, \'&lt;\', count(*), \'명&gt;\') as text ';
     }
@@ -66,7 +71,13 @@ var mailListC = {
       sql += ' search like \'%'+body.search+'%\'and ';
     }
     sql += ' M_ID = ?';
-    sql += ' group by search order by search limit ?,?';
+    sql += ' group by search';
+    if (typeof body.order !== 'undefined') {
+      sql += ' order by M_regdate desc limit ?,?';
+    }
+    else{
+      sql += ' order by search limit ?,?';
+    }
     return await getResult(sql,param);
   },
   selectViewCount: async function(body,param){
@@ -83,8 +94,32 @@ var mailListC = {
     else{
       return count[0]['total'];
     }
+  },
+  selectView2: async function(body,param){
+    var sql = 'select n_idx, search, M_id, M_group_title, M_idx_a, user_name, M_email, M_name, M_ptitle, M_tel,date_format(M_regdate, \'%Y-%m-%d %H:%i:%s\') as M_regdate FROM mail_list_group_view where M_id = ?';
+    if (typeof body.group !== 'undefined') {
+      sql += ' and M_group_title=?';
+    }
+    if (typeof body.order !== 'undefined') {
+      sql += ' order by M_regdate desc limit ?,?';
+    }
+    return await getResult(sql,param);
+  },
+  selectViewCount2: async function(body,param){
+    var sql = 'select count(*) as total FROM mail_list_group_view where M_id = ?';
+    if (typeof body.group !== 'undefined') {
+      sql += ' and M_group_title=?';
+    }
+    var count = await getResult(sql,param);
+    if(count.length == 0){
+      return 0;
+    }
+    else{
+      return count[0]['total'];
+    }
   }
 }
+
 function insertSqlSetting(keys){
   var arr = [].map.call(keys, function(obj) { return '?'; });
   columns = keys.join(', ');
--- overwareMail/routes/list.js
+++ overwareMail/routes/list.js
@@ -11,6 +11,39 @@ router.get('/',async function(req, res) {
   res.render('list',data);
 });
 
+async function getListPageData(param){
+  var data = {
+    mailList:[],
+    mailListCount:{total:0}
+  };
+  var limit = 20;
+  var searchParam = ['1',0,limit];
+  var currentPage = 1;
+  var searchBody = {
+    'order': 'n_idx'
+  };
+  if (typeof param.page !== 'undefined') {
+    currentPage = param.page;
+  }
+  if (parseInt(currentPage) > 0) {
+    searchParam[1] = (currentPage - 1) * limit
+    data['offset'] = searchParam[1];
+  }
+  if (typeof param.searchType !== 'undefined' && typeof param.search !== 'undefined') {
+    searchBody['searchType'] = param.searchType;
+    searchBody['search'] = param.search;
+  }
+  try{
+    data['mailList'] = await mailListA.selectView(searchBody,searchParam);
+    data['mailListCount'] = await mailListA.selectViewCount(searchBody,searchParam);
+    data['currentPage'] = currentPage;
+  }
+  catch(e){
+    console.log('e');
+  }
+  return data;
+}
+
 router.post('/update',async function(req, res) {
   try{
     var updateMail = await mailListA.update(req.body);
@@ -51,15 +84,6 @@ router.post('/addGroup',async function(req, res) {
   }
 });
 
-async function asyncForEach(array, callback) {
-  for (var index = 0; index < array.length; index++) {
-   var done = await callback(array[index], index, array);
-    if(done == false){
-     break;
-    }
-  }
-}
-
 router.post('/groupTitleCheck',async function(req,res){
   try{
     var titleCheck = await mailListC.titleCheck([req.body.title.replace( /(\s*)/g, ""),'1']);
@@ -79,16 +103,59 @@ router.post('/getNextPage',async function(req, res, next) {
   res.send(data);
 });
 
-async function getListPageData(param){
+async function asyncForEach(array, callback) {
+  for (var index = 0; index < array.length; index++) {
+   var done = await callback(array[index], index, array);
+    if(done == false){
+     break;
+    }
+  }
+}
+
+router.get('/group',async function(req, res) {
+  var data = await getGroupListPageData(req.query,'list');
+  res.render('listGroup',data);
+});
+
+router.post('/group/delete',async function(req, res) {
+  try{
+    var deleteMail = await mailListC.deleteFun(req.body);
+    res.send('그룹에서 삭제되었습니다.');
+  }
+  catch(e){
+    res.send('다시 시도해주세요.');
+  }
+});
+
+router.post('/getNextGroupModalPage',async function(req, res, next) {
+  // if (!req.user) {
+  //   return res.redirect('/login');
+  // }
+  var data = await getGroupListPageData(req.body,'group');
+  res.send(data);
+});
+
+router.post('/getNextGroupPage',async function(req, res, next) {
+  // if (!req.user) {
+  //   return res.redirect('/login');
+  // }
+  var data = await getGroupListPageData(req.body,'list');
+  res.send(data);
+});
+
+async function getGroupListPageData(param,type){
   var data = {
-    mailList:[],
-    mailListCount:{total:0}
+    groupList:[],
+    groupListCount:{total:0}
   };
   var limit = 20;
+  if(type == 'group'){
+    limit = 6;
+  }
   var searchParam = ['1',0,limit];
   var currentPage = 1;
   var searchBody = {
-    'order': 'n_idx'
+    'order': 'M_regdate'
   };
   if (typeof param.page !== 'undefined') {
     currentPage = param.page;
@@ -97,24 +164,28 @@ async function getListPageData(param){
     searchParam[1] = (currentPage - 1) * limit
     data['offset'] = searchParam[1];
   }
-  if (typeof param.searchType !== 'undefined' && typeof param.search !== 'undefined') {
-    searchBody['searchType'] = param.searchType;
+  if (typeof param.search !== 'undefined') {
     searchBody['search'] = param.search;
   }
   try{
-    data['mailList'] = await mailListA.selectView(searchBody,searchParam);
-    data['mailListCount'] = await mailListA.selectViewCount(searchBody,searchParam);
-    data['currentPage'] = currentPage;
+    if(type == 'list'){
+      data['groupList'] = await mailListC.selectView(searchBody,searchParam);
+      data['groupListCount'] = await mailListC.selectViewCount(searchBody,searchParam);
+      data['currentPage'] = currentPage;
+    }
+    else{
+      searchBody['group'] = param.group;
+      searchParam = [searchParam[0],param.group,searchParam[1],searchParam[2]];
+      data['groupList'] = await mailListC.selectView2(searchBody,searchParam);
+      data['groupListCount'] = await mailListC.selectViewCount2(searchBody,searchParam);
+      data['currentPage'] = currentPage;
+    }
   }
   catch(e){
     console.log('e');
   }
   return data;
 }
-
-router.get('/group', function(req, res) {
-  res.render('listGroup');
-});
 
 router.get('/add', function(req, res) {
   res.render('listAdd');
