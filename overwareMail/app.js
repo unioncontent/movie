ca50c59f662a7254fca17b12b575a32bd65b4d80
BYUN SOOYEON <smb1457@naver.com>
2018-05-08 18:27:33 +0900

    [ mail ] 메일발송 수정
    - 메일 발송페이지를 두개로 나눔( 작성, 관리)
    - 메일 발송 관리 페이지 페이징, 키워드 검색 완료
    - 메일 발송 관리 페이지 관리 기능 구현중
    - 메일 발송 작성 페이지 save 기능 구현중

--- overwareMail/app.js
+++ overwareMail/app.js
@@ -56,6 +56,7 @@ var period = require('./routes/period');
 app.use('/period', period);
 var email = require('./routes/email');
 app.use('/email', email);
+app.use('/email/manage', email);
 var list = require('./routes/list');
 app.use('/list', list);
 
