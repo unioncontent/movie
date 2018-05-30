7ab7091e9b574cb0ddc887b91fe579ef7d8ead82
WIN-HQ9QAC7PH89\Administrator <smb1457@naver.com>
2018-05-30 14:25:12 +0900

    [ mail ] editor media 수정 완료

--- overwareMail/public/pages/email/js/editor.js
+++ overwareMail/public/pages/email/js/editor.js
@@ -19,125 +19,28 @@ tinymce.init({
   remove_script_host : false,
   convert_urls : true,
   images_upload_url: '/email/send/img',
-  // images_upload_base_path: 'http://mail.overware.co.kr/',
   media_live_embeds: true,
   media_poster: false,
   media_alt_source: false,
   media_url_resolver: function (data, resolve) {
     var aTagHref = data.url;
     if(data.url.indexOf('/watch?v=') != -1 && data.url.indexOf('www.youtube.com') != -1){
-      data.url = data.url.replace('/watch?v=','/v/');
-      // data.url = data.url.replace('/watch?v=','/embed/');
+      data.url = data.url.replace('/watch?v=','/embed/');
     }
     else if(data.url.indexOf('https://youtu.be/') != -1 && data.url.indexOf('/watch?v=') == -1 && data.url.indexOf('/embed/') == -1){
-      data.url = data.url.replace('https://youtu.be/','https://www.youtube.com/v/');
-      // data.url = data.url.replace('https://youtu.be/','https://www.youtube.com/embed/');
+      data.url = data.url.replace('https://youtu.be/','https://www.youtube.com/embed/');
     }
     var embedHtml = '<div style="position: relative; text-align: center;">\
     <iframe  src="'+ data.url +'" width="640" height="360"></iframe>\
     <p style="text-align: center;">영상이 안 보이는 경우 <a target="_blank" href="'+ aTagHref +'">여기</a>를 눌러 주세요</p></div>';
     resolve({html: embedHtml});
-    // if (data.url.indexOf('YOUR_SPECIAL_VIDEO_URL') !== -1) {
-    // } else {
-    //   resolve({html: ''});s
-    // }
   },
-  // file_picker_types: 'file media',
-  // file_picker_callback: function(cb, value, meta) {
-  //   var input = document.createElement('input');
-  //   input.setAttribute('type', 'file');
-  //
-  //   if (meta.filetype == 'media') {
-  //     input.setAttribute('accept', 'video/*');
-  //   }
-  //
-  //   input.onchange = function() {
-  //     var file = this.files[0];
-  //     console.log(file);
-  //     var reader = new FileReader();
-  //     reader.readAsDataURL(file);
-  //     reader.onload = function () {
-  //       var xhr, formData;
-  //       xhr = new XMLHttpRequest();
-  //       console.log('XMLHttpRequest');
-  //       console.log(reader);
-  //       xhr.withCredentials = false;
-  //       xhr.open('POST', '/email/send/file');
-  //       // console.log(reader.result);
-  //       // console.log(file);
-  //       //
-  //       xhr.onload = function() {
-  //         var json;
-  //
-  //         if (xhr.status != 200) {
-  //           alert('HTTP Error: ' + xhr.status);
-  //           return;
-  //         }
-  //
-  //         json = JSON.parse(xhr.responseText);
-  //
-  //         if (!json || typeof json.location != 'string') {
-  //           alert('Invalid JSON: ' + xhr.responseText);
-  //           return;
-  //         }
-  //         console.log(meta.filetype);
-  //         if (meta.filetype == 'file') {
-  //           // console.log({ title: file.name});
-  //           cb(json.location, { title: file.name.split('.')[0] });
-  //         }
-  //         else if (meta.filetype == 'media') {
-  //           cb(json.location);
-  //         }
-  //       };
-  //       console.log(file, file.name);
-  //       formData = new FormData();
-  //       formData.append('file', file, file.name);
-  //       xhr.send(formData);
-  //
-  //       // $.ajax({
-  //       //   url: "/email/send/file",
-  //       //   type: "POST",
-  //       //   data: {file:reader.result},
-  //       //   dataType: 'json',
-  //       //   processData: false,
-  //       //   contentType: false,
-  //       //   success: function (data) {
-  //       //     console.log("File available at: ", data);
-  //       //     // var name = file.name.split('.')[0];
-  //       //     // var id = 'blobid' + (new Date()).getTime();
-  //       //     // var base64 = reader.result.split(',')[1];
-  //       //     // var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
-  //       //     // var blobInfo = blobCache.create(name, file, reader.result);
-  //       //     // blobCache.add(blobInfo);
-  //       //
-  //       //     if (meta.filetype == 'file') {
-  //       //       // cb(blobInfo.blobUri(), { title: file.name });
-  //       //       cb(data.location, { title: file.name });
-  //       //     }
-  //       //
-  //       //     if (meta.filetype == 'media') {
-  //       //       input.setAttribute('accept', 'video/*');
-  //       //       // cb(blobInfo.blobUri(), {source2: 'alt.ogg', poster: 'image.jpg'});
-  //       //       // cb(blobInfo.blobUri());
-  //       //       cb(data.location);
-  //       //     }
-  //       //   },
-  //       //   error: function (data) {
-  //       //     var obj = jQuery.parseJSON(data);
-  //       //     alert(obj.error);
-  //       //   }
-  //       // });
-  //     };
-  //   };
-  //   input.click();
-  // },
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css',
     '//use.fontawesome.com/releases/v5.0.13/css/all.css'
   ],
   content_style: [
-    // 'body{padding:30px; margin:auto;font-size:14px;font-family:Gulim,"Helvetica Neue",Helvetica,Arial,sans-serif;color:#222} h1,h2,h3,h4,h5,h6 {font-weight:400;margin-top:1.2em} h1 {} h2{} table {width:100%; border-collapse: collapse;} table td, th {border: 1px solid; padding:10px; text-align:left;font-size:14px;font-family:Gulim,"Helvetica Neue",Helvetica,Arial,sans-serif;} table th {background-color:#E2E4E7}'
     '.mce-content-body {padding:30px; margin:auto; padding-top:25px; font-size: 12px; font-family: 돋움!important;color: #333333;}.mce-content-body p{margin: 0;padding: 0;}'
   ],
   setup: function(editor) {
@@ -155,7 +58,6 @@ tinymce.init({
     });
 
     editor.addButton('spaceline', {
-      // type: 'listbox',
       type: 'menubutton',
       icon: false,//'fa fa-arrows-v'
       text: '줄간격',
@@ -251,7 +153,6 @@ tinymce.init({
     });
   },
   init_instance_callback: function(editor){
-    // var defaultHtml= '<table width="750" align="center" cellpadding="0" cellspacing="0" style="border: solid 1px #cacaca; padding: 20px;"><tbody><tr><td><table width="100%" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td width="642"><img src="http://showbox.email/templates/images/logo/show_logo.png" width="135" height="36" alt="로고"></td><td width="92">NEWS No.</td></tr></tbody></table><table width="100%" border="0" cellpadding="0" cellspacing="0"><tbody><tr><td><p><br></p><div style="text-align: center; margin-top: 100px; padding: 10px; background-color: #eeeeee"></div><br><div style="padding: 40px 20px;"><p style="font-size: 13px; font-weight:bold; color: #000; padding: 0 0 5px 3px"> 영화 관련 데이타 다운로드</p><hr><br></div><p><br></p></td></tr></tbody></table></td></tr></tbody></table>';
     var defaultHtml = '<p><br></p><div style="text-align: center; margin-top: 100px; padding: 10px; background-color: #eeeeee"></div><br><div style="padding: 40px 20px;"><p style="font-size: 13px; font-weight:bold; color: #000; padding: 0 0 5px 3px"> 영화 관련 데이타 다운로드</p><hr><br></div><p><br></p>'
     if($('#mailType').val()!=''){
       var mailData = JSON.parse($("#mailData").val());
