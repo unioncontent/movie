aa687cd96fef54c942b42a40cc157ac1e5e75c19
BYUN SOOYEON <smb1457@naver.com>
2018-06-20 19:09:00 +0900

    [ mail ] 뉴스클리핑 계정 - 선택기사 리스트 기사입력 모달 개발중

--- overwareMail/routes/news.js
+++ overwareMail/routes/news.js
@@ -12,7 +12,7 @@ var isAuthenticated = function (req, res, next) {
   }
   res.redirect('/login');
 };
-
+// 기사 선택 페이지
 router.get('/',isAuthenticated,async function(req, res) {
   var data = await getListPageData(req.user.user_admin,req.query);
   data.klist = await keyword.selectMovieKwdAll(req.user.user_admin,req.user.n_idx) || [];
@@ -21,6 +21,7 @@ router.get('/',isAuthenticated,async function(req, res) {
   data.keyword = '';
   res.render('news',data);
 });
+
 router.post('/getNextPage',isAuthenticated,async function(req, res, next) {
   try{
     var data = await getListPageData(req.user.user_admin,req.body);
@@ -29,6 +30,7 @@ router.post('/getNextPage',isAuthenticated,async function(req, res, next) {
     res.status(500).send(e);
   }
 });
+
 async function getListPageData(idx,param){
   var data = {
     list:[],
@@ -96,4 +98,85 @@ async function asyncForEach(array, callback) {
     }
   }
 }
+
+// 선택 기사 리스트 페이지
+router.get('/list',isAuthenticated,async function(req, res) {
+  var data = await getListPageData2(req.user.user_admin,req.query);
+  data.klist = await keyword.selectMovieKwdAll(req.user.user_admin,req.user.n_idx) || [];
+  data.sDate = '';
+  data.eDate = '';
+  data.keyword = '';
+  res.render('newsList',data);
+});
+
+router.post('/list/getNextPage',isAuthenticated,async function(req, res, next) {
+  try{
+    var data = await getListPageData2(req.user.user_admin,req.body);
+    res.send({status:true,result:data});
+  } catch(e){
+    res.status(500).send(e);
+  }
+});
+
+router.post('/list/delete',isAuthenticated,async function(req, res, next) {
+  try{
+    await newsclipping.delete(req.body.idx);
+    res.send({status:true});
+  } catch(e){
+    res.status(500).send(e);
+  }
+});
+
+router.post('/list/update',isAuthenticated,async function(req, res, next) {
+  try{
+    await newsclipping.update([req.body.thumbnail,req.body.thumbnail_code,req.body.idx]);
+    res.send({status:true});
+  } catch(e){
+    res.status(500).send(e);
+  }
+});
+
+async function getListPageData2(idx,param){
+  var data = {
+    list:[],
+    listCount:{total:0},
+    sDate: '',
+    eDate: '',
+    keyword: '',
+    page: 1
+  };
+  var limit = 10;
+  var searchParam = [idx,0,limit];
+  var currentPage = 1;
+  var searchBody = {};
+  if (typeof param.page !== 'undefined') {
+    currentPage = param.page;
+    data['page'] = currentPage;
+  }
+  if (parseInt(currentPage) > 0) {
+    searchParam[1] = (currentPage - 1) * limit
+    data['offset'] = searchParam[1];
+  }
+  if (typeof param.sDate !== 'undefined' && typeof param.eDate !== 'undefined') {
+    searchBody['sDate'] = param.sDate;
+    searchBody['eDate'] = param.eDate;
+    data['sDate'] = param.sDate;
+    data['eDate'] = param.eDate;
+  }
+  if (typeof param.keyword !== 'undefined') {
+    searchBody['keyword'] = param.keyword;
+    data['keyword'] = param.keyword;
+  }
+  try{
+    data['list'] = await newsclipping.selectNewsMailTable(searchBody,searchParam);
+    data['listCount'] = await newsclipping.selectNewsMailTableCount(searchBody,searchParam);
+    data['currentPage'] = currentPage;
+  }
+  catch(e){
+    console.log('e');
+  }
+  return data;
+}
+
+
 module.exports = router;
