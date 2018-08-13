f16117074badd62a01bdc5be294a2452fed106a6
BYUN SOOYEON <smb1457@naver.com>
2018-08-13 16:49:43 +0900

    [ mail ] 키워드 관리 추가, 서식복사 코드 제거

--- overwareMail/models/keyword.js
+++ overwareMail/models/keyword.js
@@ -2,35 +2,74 @@ const DBpromise = require('../db/db_info.js');
 
 /*
  키워드 테이블 - keyword_data
+ 메일 키워드 테이블 - m_keyword_data
 */
 
 var keyword = {
   selectMovieKwd: async function(admin,param){
-    if(admin != null){
-      param = admin;
+    var sql = 'SELECT * FROM m_keyword_data where user_idx=? and keyword_property=1';
+    return await getResult(sql,param);
+  },
+  selectKwdList: async function(body,param){
+    var sql = 'SELECT keyword_idx, user_idx, keyword_property, keyword_main,  DATE_FORMAT(createDate, \'%Y-%m-%d %H:%i:%s\') AS createDate, DATE_FORMAT(updateDate, \'%Y-%m-%d %H:%i:%s\') AS updateDate FROM m_keyword_data where user_idx=?';
+    if('keyword' in body){
+      sql +=' and keyword_main like \'%'+body.keyword+'%\'';
     }
-    // showbox = 1
-    param = 1;
-    var sql = 'SELECT * FROM keyword_data where user_idx=? and keyword_property=\'포함\' group by keyword_main';
+    sql += ' order by keyword_idx desc limit ?,?';
+    return await getResult(sql,param);
+  },
+  selectKwdListCount: async function(body,param){
+    var sql = 'SELECT count(*) as total FROM m_keyword_data where user_idx=?';
+    if('keyword' in body){
+      sql +=' and keyword_main like \'%'+body.keyword+'%\'';
+    }
+    sql += ' order by keyword_idx desc';
+    var count = await getResult(sql,param);
+    if(count.length == 0){
+      return 0;
+    }
+    else{
+      return count[0]['total'];
+    }
+  },
+  insert: async function(table,param){
+    var pValue = Object.values(param);
+    var sql = insertSqlSetting(table,Object.keys(param));
+    return await getResult(sql,pValue);
+  },
+  addKwdCheck: async function(param) {
+    var sql = 'select * from m_keyword_data where keyword_main=?';
+    return await getResult(sql,param);
+  },
+  update: async function(param){
+    var pValue = Object.values(param);
+    var sql = 'update m_keyword_data set keyword_main=? where keyword_idx=?';
+    return await getResult(sql,pValue);
+  },
+  updateState: async function(param){
+    var pValue = Object.values(param);
+    var sql = 'update m_keyword_data set keyword_property=? where keyword_idx=?';
+    return await getResult(sql,pValue);
+  },
+  delete: async function(param){
+    var sql = 'delete from m_keyword_data where keyword_idx=?';
     return await getResult(sql,param);
   },
   selectMovieKwdAll: async function(admin,param){
     if(admin != null){
       param = admin;
     }
-    var value = [param,param];
-    var sql = '(SELECT keyword_main as M_keyword,keyword_idx as M_keyword_idx FROM keyword_data where user_idx=? and keyword_property=\'포함\' group by keyword_main) union\
-              (SELECT M_keyword,M_keyword_idx FROM period_view where ';
-    if(admin == null){
-      sql += ' M_id=? or M_id in (select n_idx from m_mail_user where user_admin=?) ';
-      value.push(param);
-    }
-    else{
-      sql += ' M_id=? ';
-    }
-    sql += 'group by M_keyword)';
-    return await getResult(sql,value);
+    var sql = 'select * from m_keyword_data where user_idx=?';
+    return await getResult(sql,param);
   }
+}
+
+function insertSqlSetting(table,keys){
+  var arr = [].map.call(keys, function(obj) { return '?'; });
+  columns = keys.join(', ');
+  placeholders = arr.join(', ');
+  var sql = "INSERT INTO "+table+" ( "+columns+" ) VALUES ( "+placeholders+" );";
+  return sql;
 }
 
 async function getResult(sql,param) {
---
+++ overwareMail/routes/keyword.js
@@ -0,0 +1,123 @@
+var express = require('express');
+var router = express.Router();
+// DB module
+var keyword = require('../models/keyword.js');
+
+var isAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+};
+
+router.get('/',isAuthenticated,async function(req, res) {
+  var data = await getListPageData(req.user.n_idx,req.query);
+  res.render('keyword',data);
+});
+
+router.post('/getNextPage',isAuthenticated,async function(req, res) {
+  try{
+    var data = await getListPageData(req.user.n_idx,req.body);
+    res.send({status:true,list:data});
+  }
+  catch(e){
+    res.status(500).send(e);
+  }
+});
+
+async function getListPageData(idx,param){
+  var data = {
+    list:[],
+    listCount:0,
+    page:1,
+    keyword:''
+  };
+  var limit = 20;
+  var searchParam = [idx,0,limit];
+  var currentPage = 1;
+  var searchBody = {};
+  if (typeof param.keyword !== 'undefined') {
+    searchBody['keyword'] = param.keyword;
+    data['keyword'] = searchBody['keyword'];
+  }
+  if (typeof param.page !== 'undefined') {
+    currentPage = param.page;
+    data['page'] = currentPage;
+  }
+  if (parseInt(currentPage) > 0) {
+    searchParam[1] = (currentPage - 1) * limit
+    data['offset'] = searchParam[1];
+  }
+
+  try{
+    data['list'] = await keyword.selectKwdList(searchBody,searchParam);
+    data['listCount'] = await keyword.selectKwdListCount(searchBody,searchParam);
+    data['currentPage'] = currentPage;
+  }
+  catch(e){
+    console.log(e);
+  }
+  return data;
+}
+
+router.post('/add/kwdCheck',isAuthenticated,async function(req, res){
+  try{
+    var kwdCheck = await keyword.addKwdCheck([req.body.kwd]);
+    var result = {msg: (kwdCheck.length == 0) ? 'success':'fail'};
+    res.send(result);
+  }
+  catch(e){
+    res.status(500).send(e);
+  }
+});
+
+router.post('/update',isAuthenticated,async function(req, res) {
+  try{
+    var result;
+    if('state' in req.body){
+      result = await keyword.updateState(req.body);
+    }
+    else{
+      result = await keyword.update(req.body);
+    }
+    if('changedRow' in result){
+      res.status(500).send(e);
+      return false;
+    }
+    res.send({status:true});
+  }
+  catch(e){
+    res.status(500).send(e);
+  }
+});
+
+router.post('/delete',isAuthenticated,async function(req, res) {
+  try{
+    var result = await keyword.delete(req.body.idx);
+    if('changedRow' in result){
+      res.status(500).send(e);
+      return false;
+    }
+    res.send({status:true});
+  }
+  catch(e){
+    res.status(500).send(e);
+  }
+});
+
+router.post('/add',isAuthenticated,async function(req, res) {
+  try{
+    var param = {
+      user_idx:req.user.n_idx,
+      keyword_property:req.body.property,
+      keyword_main:req.body.keyword
+    };
+    await keyword.insert('m_keyword_data',param);
+    res.send({status:true});
+  }
+  catch(e){
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
