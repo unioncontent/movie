1d1ebdf9ca8c330037abc02280d2e9a393faead6
BYUN SOOYEON <smb1457@naver.com>
2018-09-05 15:53:37 +0900

    [ mail ] 서버구동 코드 수정

--- overwareMail/models/maillink.js
+++ overwareMail/models/maillink.js
@@ -96,7 +96,7 @@ var mail = {
   selectResultDetail2: async function(param){
     // ,l.M_ptitle
     // left join m_mail_list_all as l on a.EMTOADDRESS = l.M_email
-    var sql = 'select a.MSGID,m.M_type,k.keyword_main as M_keyword_str,m.M_mail_type,m.M_invitation,m.M_seq_number,m.M_subject,a.EMTONAME,a.EMTOADDRESS,a.SENDTIME,a.FINALRESULT,m.M_regdate as GENDATE,a.OPENTIME\
+    var sql = 'select a.MSGID,l.M_ptitle,m.M_type,k.keyword_main as M_keyword_str,m.M_mail_type,m.M_invitation,m.M_seq_number,m.M_subject,a.EMTONAME,a.EMTOADDRESS,a.SENDTIME,a.FINALRESULT,m.M_regdate as GENDATE,a.OPENTIME\
     from (SELECT * FROM `union`.mail_send_backup where MSGID=? and ';
     if(param[1] == 13){
       sql +='((FINALRESULT=13) or (FINALRESULT is null and PROCESSYN = \'Y\'))';
@@ -106,6 +106,7 @@ var mail = {
     }
     sql +=') as a left join m_mail_all_a as m\
     on a.MSGID = m.n_idx\
+    left join m_mail_list_all as l on a.EMTOADDRESS = l.M_email\
     left join m_keyword_data as k\
     on m.M_keyword = k.keyword_idx';
     return await getResult(sql,param[0]);
