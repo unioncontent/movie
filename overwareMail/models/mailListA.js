3396e62d84946f080c84ca0667bba084f7cbe474
BYUN SOOYEON <smb1457@naver.com>
2018-10-10 19:05:23 +0900

    [ mail ] 기능 추가 및 수정
    - 메일 작성에 메일 불러오기 기능 추가
    - 재작성 버튼 클릭시 정보 유지

--- overwareMail/models/mailListA.js
+++ overwareMail/models/mailListA.js
@@ -57,6 +57,21 @@ var mailListAll = {
     var result = await getResult(sql,[]);
     return [].map.call(result, function(obj) { return [obj.M_name,obj.M_email.replace('\r','')]; });
   },
+  getOneEmail3 : async function(param){
+    var sql = 'select n_idx,M_name,M_email from m_mail_list_all ';
+    if(typeof param == 'object'){
+      sql += 'where n_idx ='+param[0];
+      for(var i = 1; i < param.length; i++){
+        sql += ' or';
+        sql += ' n_idx ='+param[i];
+      }
+    }
+    else if(typeof param == 'string'){
+      sql += 'where n_idx ='+param;
+    }
+    var result = await getResult(sql,[]);
+    return [].map.call(result, function(obj) { return [obj.n_idx,obj.M_name+'&lt;'+obj.M_email.replace('\r','')+'&gt;']; });
+  },
   getOneInfo : async function(n_idx){
     var sql = 'select * from m_mail_list_all where n_idx=?';
     return await getResult(sql,[n_idx]);
